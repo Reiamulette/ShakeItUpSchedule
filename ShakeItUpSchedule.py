0b29ce3693e6d9a560f63035de9934257523fbf9
Reiamulette <yao6488@gmail.com>
2019-11-17 21:10:26 -0500

    Create ShakeItUpSchedule.py

---
+++ ShakeItUpSchedule.py
@@ -0,0 +1,8 @@
+#noinspection PyUnresolvedReferences
+import pandas as pd
+#noinspection PyUnresolvedReferences
+import numpy as np
+
+
+with open(input(),'rU') as input_file:
+pd.read_excel(input_file)
