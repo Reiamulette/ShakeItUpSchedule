8d8f7f7dcfb6d39b73e8d570fa1bc8289514bc32
Reiamulette <yao6488@gmail.com>
2019-12-01 02:31:19 -0500

    cleaner(?)

--- schedule.py
+++ schedule.py
@@ -1,22 +1,32 @@
-#noinspection PyUnresolvedReferences
+# noinspection PyUnresolvedReferences
 import pandas as pd
-#noinspection PyUnresolvedReferences
+# noinspection PyUnresolvedReferences
 import numpy as np
-#noinspection PyUnresolvedReferences
+# noinspection PyUnresolvedReferences
 import xlrd
-#noinspection PyUnresolvedReferences
+# noinspection PyUnresolvedReferences
 import time
-#noinspection PyUnresolvedReferences
+# noinspection PyUnresolvedReferences
 import datetime as dt
-#noinspection PyUnresolvedReferences
+# noinspection PyUnresolvedReferences
 import timedelta as td
+global df
+global dateofday
+global dateofdaya #creates list of dates
+global start_time
+global start_timea
+global end_timea
+global end_time
+global interest
+global b
+global category
+global is_day
+global is_category
 
 
-b = []
-category = []
 def load_data():
+    globals()
     print("Welcome to ShakeItUpSchedule. \n Please make sure the excel file includes the following categories in the columns: \n Serial Number, Event Title, Event Description, Day, Location, Start Time, End Time, Categories, and Subcategories")
-    global df
     while True:
         try:
             filename = input("Enter the path of the excel file: ")
@@ -27,17 +37,12 @@ def load_data():
             continue
         break
 
-    return df
+
 def day_time():
     day = int(input("How many days of the event are you planning to attend?"))
     print(day)
 
-    global dateofday
-    global dateofdaya #creates list of dates
-    global start_time
-    global start_timea
-    global end_timea
-    global end_time
+
     dateofdaya = []
     start_timea = [] #creates list of start time
     end_timea = []   #creates list of end time
@@ -49,6 +54,7 @@ def day_time():
                 dateofday = dt.datetime.strptime(dateofday,"%m/%d/%Y")
                 dateofday.strftime("%m/%d/%Y")
                 dateofdaya.append(dateofday.strftime("%m/%d/%Y"))
+                print(dateofdaya)
             except:
                 print("\nERROR! Please enter the date of day " + str(x+1) +" in MM/DD/YYYY format. \nPossible Errors may include:\n- Missing slashes\n")
                 continue
@@ -83,56 +89,55 @@ def day_time():
                 continue
             break
     print("\n\n")
-    print ("Arrival Time: ", start_timea)
-    print ("End Time: ", end_timea)
+    print("Arrival Time: ", start_timea)
+    print("End Time: ", end_timea)
     print("\n Noted. \n")
-
-    return start_time
-    return end_time
-
-def filter_date():
-    global b
-    global c
-    global possible_events
-    for x in range(len(dateofdaya)):
-        possible_events = df["Day"].isin(b)
 
 
 def survey():
-    global a
-    global b
-    global category
-    global is_day
+
+    b = []
     category = ["Artist Alley", "Dealer's Room", "Featured Panels", "Arcade", "Manga Library", "Contests", "Maid Cafe" , "Guest Autographs", "Concerts"]
     global possible_events
     global possible_df
     possible_events = []
     possible_df = pd.DataFrame()
     for x in category:
-        a = input("Are you interested in " + str(x) + "?\nEnter yes or no.")
-        if a == "yes":
+        interest = input("Are you interested in " + str(x) + "?\nEnter yes or no.")
+        if interest == "yes":
             is_category = df["Categories"].isin([x])
-            print(df[is_category])
-            b.append(df[is_category])
+
+            pd.b.add(is_category)
+            print(is_category)
+            print("is_category")
+            print(type(is_category))
+            print("b")
+            print(type(b))
+
             filter_date()
+
         elif a == "no":
-            False
+            pass
         else:
             print("Not an accepted answer.")
             break
-    print(b)
+        print(type(b))
 
 
 
+def filter_date():
+    global b
+    global possible_events
 
-
+    for x in dateofdaya:
+        #possible_events = is_category.df["Day"].isin([b])
+        print("possible_events")
+        print(type(possible_events))
 
 
 def event_on():
     print(df["Start Time"])
     print(start_time)
-
-
 
 #def createSched():
 
